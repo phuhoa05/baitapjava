65080c465a38e2738a22fb12632d216b1b061af7
Administrator <Administrator@VIPA-03>
2024-08-24 17:50:11 +0700

    Nguyen Phu Hoa nop bai

---
+++ src/baitap/Baitap.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitap;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Baitap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=15;
+        int b=4;
+        int add= a+b;
+        System.out.println("add ="+ add);
+        int sub = a-b;
+        System.out.println("sub ="+sub);
+        int mul =a*b;
+        System.out.println("mul ="+mul);
+        int div = a/b;
+        System.out.println("div ="+div);
+        int remaider = a%b;
+        System.out.println("remaider ="+remaider);
+    }
+    
+}
